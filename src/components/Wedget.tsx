a096ae0c0986bd1adceccf89e900115fddcca7d2
Andrelino Silva <andrelinodev@gmail.com>
2022-05-02 19:42:32 -0300

    Add: Popover @headlessui/react

--- src/components/Wedget.tsx
+++ src/components/Wedget.tsx
@@ -3,22 +3,20 @@ import { useState } from 'react';
 import { Popover } from '@headlessui/react';
 
 export function Widget() {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
     return (
         <div className="w-screen h-screen bg-gray-500">
-            <div className="absolute bottom-5 right-5 ">
-                {isWidgetOpen && <p className="center">Hello World</p>}
-                <button
+            <Popover className="absolute bottom-5 right-5 ">
+                <Popover.Panel>Hello World</Popover.Panel>
+                <Popover.Button
                     className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group "
-                    onClick={() => setIsWidgetOpen(!isWidgetOpen)}
+                    // onClick={() => setIsWidgetOpen(!isWidgetOpen)}
                 >
                     <ChatTeardropDots className="w-6 h-6" />
                     <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
                         Feedback
                     </span>
-                </button>
-            </div>
+                </Popover.Button>
+            </Popover>
         </div>
     );
 }
