16064ec29136330b4dd6c543f6e9e64c596b2453
Andrelino Silva <andrelinodev@gmail.com>
2022-05-13 12:17:42 -0300

    Up

--- src/components/Feedget/WidgetForm/ScreenShotButton/index.tsx
+++ src/components/Feedget/WidgetForm/ScreenShotButton/index.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import html2canvas from "html2canvas";
 import { Camera, Eye, Trash } from "phosphor-react";
 import { Loading } from "../../Loading";
-import { ModalScreen } from "../../Modal";
+
 import Modal from "react-modal";
 
 export interface ScreenshotButtonProps {
